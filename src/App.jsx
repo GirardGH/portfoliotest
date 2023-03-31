7527a276452bca70c90ae2250485aa613cb77e5f
GIRARD GHISLAIN <girard.gh@gmail.com>
2023-03-31 14:32:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Contact from "./components/Contact"
+import Hero from "./components/Hero"
+import Who from "./components/Who"
+import Works from "./components/Works"
+import styled from "styled-components"
+
+const Container = styled.div`
+height : 100vh;
+background-color : yellow;
+`
+
+function App() {
+
+  return (
+    <Container>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </Container>
+  )
+}
+
+export default App
