c0d3e2e0a6681902e23481747e5ddc797a3fcf36
Girard Ghislain <girard.gh@gmail.com>
2023-04-11 13:56:13 +0200

    integration sphere

---
+++ src/components/ProductDesign.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductDesign = () => {
+  return (
+    <div>WebDesign</div>
+  )
+}
+
+export default ProductDesign
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import ProductDesign from './ProductDesign';
+import Development from './Development';
+import WebDesign from './WebDesign';
 
 const data = [
   "Web Design",
-  "Mobile App",
-  "Branding",
-  "Marketing",
-  "Photography",
+  "Development",
+  "illustration",
+  "Product Design",
+  "Social Media",
 ]
 
 const Section = styled.div`
@@ -73,17 +76,29 @@ const Right = styled.div`
 `;
 
 const Works = () => {
+
+  const [work, setWork] = useState("Web Design");
   return (
     <Section>
       <Container>
         <Left>
           <List>
             {data.map((item) => (
-              <ListItem key={item} text={item}>{item}</ListItem>
+              <ListItem key={item} text={item} onClick={() => setWork(item)}>
+                {item}
+              </ListItem>
             ))}
             </List>
         </Left>
-        <Right></Right>
+        <Right>
+          {work === "Web Design" ? (
+            <WebDesign />
+          ) : work === "Development" ? (
+            <Development />
+          ) : ( 
+            <ProductDesign />
+          )}
+        </Right>
       </Container>
     </Section>
   )
