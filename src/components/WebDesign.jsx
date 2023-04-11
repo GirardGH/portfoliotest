91bcb0172abc19e971a566153c6f552f1fbedd2c
GIRARD GHISLAIN <girard.gh@gmail.com>
2023-04-11 23:37:48 +0200

    fix pb

--- src/components/WebDesign.jsx
+++ src/components/WebDesign.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Stage } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import Chair from './Chair'
@@ -6,7 +6,10 @@ import Chair from './Chair'
 const WebDesign = () => {
   return (
     <Canvas>
+      <Stage environment="city" intensity={0.6}>
+
       <Chair />
+      </Stage>
       <OrbitControls enableZoom={false} />
     </Canvas>
   )
